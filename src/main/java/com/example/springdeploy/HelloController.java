acdedb56e39a6790ae66e8208376a547ad728aea
magase <32868460+magase@users.noreply.github.com>
2022-09-28 16:45:00 +0200

    first commit
    first commit

---
+++ src/main/java/com/example/springdeploy/HelloController.java
@@ -0,0 +1,44 @@
+package com.example.springdeploy;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloController {
+
+    @Value("${app.message}")
+    String message;
+    /*
+    @Value("${app.varexample}")
+    String variableEntorno;
+    */
+    @GetMapping("/hola")
+    public String holaMundo() {
+        System.out.print(message);
+        //System.out.print(variableEntorno);
+        return "Hola mundo que tal vamos!";
+    }
+
+    @GetMapping("/bootstrap")
+    public String bootstrap() {
+
+        return """
+                <!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <title>Bootstrap demo</title>
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+                  </head>
+                  <body>
+                    <h1>Hello, world!</h1>
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+                  </body>
+                </html>
+                """;
+
+    }
+}
